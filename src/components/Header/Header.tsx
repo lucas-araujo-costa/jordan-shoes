a600b26cc8600c7ad86726f27824e62875781fed
Lucas <lucasaraujo1964@gmail.com>
2024-03-22 10:01:42 -0300

    feat: adding responsiveness

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header className='bg-base-white h-16 flex justify-center items-center'>
-      <span className="text-2xl font-medium">Frete grátis para todo o Brasil</span>
-    </header>
+    <div className='bg-base-white flex justify-center items-center h-[2.4375rem] md:h-[2.5625rem] xl:h-16'>
+      <span className="font-medium text-sm md:text-base xl:text-2xl">Frete grátis para todo o Brasil</span>
+    </div>
   )
 }
