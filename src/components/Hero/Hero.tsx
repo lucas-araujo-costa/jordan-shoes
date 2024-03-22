bce5351b30851897ab7aa7e5040e9203e83f8d17
Lucas <lucasaraujo1964@gmail.com>
2024-03-22 12:24:46 -0300

    feat: responsive design in hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,15 +2,15 @@ import vector from "../../assets/img/Hero/vector.png"
 
 export default function Hero() {
   return (
-    <section className="bg-base-image h-96 flex items-center bg-no-repeat">
-      <div className="text-white ml-80 flex flex-col gap-10 w-[34.8125rem]">
+    <section className="bg-base-image flex items-center bg-no-repeat bg-center bg-cover h-[350px] lg:h-[400px]">
+      <div className="text-white flex flex-col gap-10 mx-6 my-auto md:ml-8 md:w-[557px] xl:ml-[352px]" >
         <div className="flex gap-3 items-center">
-          <img src={vector} alt="Ícone do Jordan" />
-          <h1 className="text-2xl font-medium">JordanShoes</h1>
+          <img src={vector} alt="Ícone do Jordan" className="size-6" />
+          <h1 className="font-medium text-lg xl:text-2xl">JordanShoes</h1>
         </div>
-        <div className="flex flex-col gap-5 ">
-          <h2 className="text-4xl font-medium">A melhor loja de Jordan</h2>
-          <p className="text-2xl font-normal">O tênis Jordan é fruto de uma velha e forte
+        <div className="flex flex-col gap-3">
+          <h2 className="font-medium text-2xl xl:text-4xl">A melhor loja de Jordan</h2>
+          <p className="font-normal text-base md:text-lg xl:text-2xl">O tênis Jordan é fruto de uma velha e forte
             parceria entre a Nike e o jogador Michael Jordan.</p>
         </div>
       </div>
